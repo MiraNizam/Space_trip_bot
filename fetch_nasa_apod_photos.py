18488300f359ecd40d17d96c27bbd3fdef176d33
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2022-11-13 15:19:14 +0300

    Create fetch_nasa_apod_photos.py

---
+++ fetch_nasa_apod_photos.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def fetch_nasa_apod(api_key: str, limit: int = 5) -> list:
+    """Get list of image urls from the NASA APOD API"""
+    nasa_api_apod = "https://api.nasa.gov/planetary/apod"
+    params = {"api_key": api_key,
+              "count": limit}
+    response = requests.get(nasa_api_apod, params=params)
+    response.raise_for_status()
+    apod_photos = response.json()
+    image_urls = []
+
+    for photo in apod_photos:
+        image = photo['hdurl']
+        image_urls.append(image)
+    return image_urls
