2987e00e5a15519c8b25c682d7d25334ad084cb1
Эльмира Низамова <e.nizamova@integra.local>
2022-11-16 11:12:30 +0300

    Publish photo to Telegram channel.

--- telegram_api.py
+++ telegram_api.py
@@ -1,8 +1,9 @@
 import telegram
 from environs import Env
+from telegram import InputMediaPhoto
 
 env = Env()
 env.read_env()
 
 bot = telegram.Bot(env.str("TG_TOKEN"))
-bot.send_message(text="Hi, I'm space!", chat_id="@space_trip_channel")
+bot.send_document(chat_id="@space_trip_channel", document=open("images/nasa_apod_photos/2.jpg", "rb"))
