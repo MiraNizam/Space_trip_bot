87cf5d901b27fdf826fa1c9e2055c1c67b21de20
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2022-12-30 20:41:37 +0300

    Add def: change photo size

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from itertools import islice
 from os.path import splitext
 from pathlib import Path
@@ -6,6 +7,8 @@ from urllib.parse import unquote, urlparse
 
 import requests
 from environs import Env
+from PIL import Image
+
 from fetch_nasa_apod_photos import fetch_nasa_apod
 from fetch_nasa_epic_photos import fetch_nasa_epic
 from fetch_spacex_photos import fetch_spacex_launch
@@ -18,10 +21,10 @@ def parser_cmd_args():
     """Parser information add information to func"""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--launch_id",
+        "launch_id",
         type=str,
         default="latest",
-        help="Input the launch_id, if you miss it, func will use 'latest' launch"
+        help="Input the launch_id, if you miss it, func will use 'latest' launch",
     )
     return parser.parse_args()
 
@@ -36,7 +39,7 @@ def get_file_extension(url_photo: str) -> str:
 def download_photos(image_urls: list, path: str, params: dict = {}) -> None:
     """Download photos and safe them in folder"""
     Path(path).mkdir(parents=True, exist_ok=True)
-    for image_number, url in islice(enumerate(image_urls, 1), 0, 5):
+    for image_number, url in islice(enumerate(image_urls, 1), 0, 25):
         filename = f"{image_number}{get_file_extension(url)}"
         response = requests.get(url, params=params)
         response.raise_for_status()
@@ -45,10 +48,24 @@ def download_photos(image_urls: list, path: str, params: dict = {}) -> None:
             file.write(response.content)
 
 
-if __name__ == '__main__':
+def change_picture_size(path: str = "images/"):
+    """Reduces photos while maintaining aspect ratio."""
+    for root, dir, files in os.walk(path):
+        for picture in files:
+            with Image.open(f"{root}/{picture}") as file:
+                file.thumbnail((1280, 1280))
+                file.save(f"{root}/{picture}")
+
+
+if __name__ == "__main__":
     args = parser_cmd_args()
     launch_id = args.launch_id
-    download_photos(fetch_spacex_launch(launch_id), "images/space_x_photos/")
     NASA_KEY = env.str("NASA_KEY")
+    download_photos(fetch_spacex_launch(launch_id), "images/space_x_photos/")
     download_photos(fetch_nasa_apod(NASA_KEY), "images/nasa_apod_photos/")
-    download_photos(fetch_nasa_epic(NASA_KEY), "images/nasa_epic_photos/", {"api_key": NASA_KEY})
+    download_photos(
+        fetch_nasa_epic(NASA_KEY),
+        "images/nasa_epic_photos/",
+        {"api_key": NASA_KEY}
+    )
+    change_picture_size()
