4d3632d2762c24a4d64adbce1d5de5aa6c485f94
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2022-12-30 20:48:54 +0300

    Add: parser for sleep,  sending photos to telegram

--- telegram_api.py
+++ telegram_api.py
@@ -1,9 +1,37 @@
+import argparse
+import os
+import os.path
+import time
+
 import telegram
 from environs import Env
-from telegram import InputMediaPhoto
 
-env = Env()
-env.read_env()
 
-bot = telegram.Bot(env.str("TG_TOKEN"))
-bot.send_document(chat_id="@space_trip_channel", document=open("images/nasa_apod_photos/2.jpg", "rb"))
+def send_to_bot(token: str, chat_id: str, sleep: int, path: str = "images/") -> None:
+    """send photos to telegram by one piece with the specified interval"""
+    bot = telegram.Bot(token=token)
+    while True:
+        for root, dir, files in os.walk(path):
+            for picture in files:
+                with open(f"{root}/{picture}", "rb") as file:
+                    bot.send_photo(chat_id=chat_id, photo=file)
+                time.sleep(sleep)
+
+
+if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "interval_in_seconds",
+        type=int,
+        default=14400,
+        help="you can specify the interval in seconds to publish photos, default is 14440",
+    )
+    namespace = parser.parse_args()
+
+    sleep = int(namespace.interval_in_seconds)
+    token = env.str("TG_TOKEN")
+    chat_id = env.str("TG_CHAT_ID")
+    send_to_bot(token, chat_id, sleep)
