851dfa2d9d2d6acdfc867839668a23f0fb6964b8
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-01-01 14:04:27 +0300

    Rename variable

--- fetch_spacex_photos.py
+++ fetch_spacex_photos.py
@@ -1,15 +1,15 @@
 import requests
 
 
-def fetch_spacex_launch(launch_id: str) -> list:
+def fetch_spacex_launch(launch_id: str="latest") -> list:
     """Get list of image urls from the SpaceX API request"""
     spacex_api_launches = "https://api.spacexdata.com/v5/launches/"
     payload = {"id": launch_id}
     response = requests.get(spacex_api_launches, params=payload)
     response.raise_for_status()
-    launch = response.json()
+    links = response.json()
     image_urls = []
-    for images in launch:
+    for images in links:
         image = images['links']['flickr']['original']
         if image:
             image_urls.extend(image)
